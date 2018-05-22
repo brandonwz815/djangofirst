0b16eb8e05563a08696f88a2e7a43894a49935a1
Brandon Zhu <brandon.zhu@utraque.com>
2018-05-21 21:40:19 -0400

    end of episode 13

--- trydjango1-11/src/restraunts/views.py
+++ trydjango1-11/src/restraunts/views.py
@@ -1,6 +1,8 @@
 import random
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 
 # Create your views here.
 def home(request):
@@ -29,3 +31,30 @@ def contact(request):
     context = {
     }
     return render(request, "contact.html", context) 
+  
+class HomeView(TemplateView):
+    template_name = "home.html"  
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = None
+        some_list = [
+            random.randint(0, 1000000), 
+            random.randint(0, 1000000), 
+            random.randint(0, 1000000)
+        ]
+        condition_bool_item = True
+        if condition_bool_item:
+            num = random.randint(0, 1000000)
+        context = {
+            "bool_item": True,
+            "num": num, 
+            "some_list": some_list
+        }
+        return context
+
+# class AboutView(TemplateView):
+#     template_name = "about.html"
+
+# class ContactView(TemplateView):
+#     template_name = "contact.html"
