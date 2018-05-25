a79c9fefebc8b5084357a16df34d8cbc206915f4
Brandon Zhu <brandon.zhu@utraque.com>
2018-05-25 00:12:22 -0400

    end of episode 16

--- trydjango1-11/src/restraunts/views.py
+++ trydjango1-11/src/restraunts/views.py
@@ -1,60 +1,14 @@
-import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 
-# Create your views here.
-def home(request):
-    num = None
-    some_list = [
-        random.randint(0, 1000000), 
-        random.randint(0, 1000000), 
-        random.randint(0, 1000000)
-    ]
-    condition_bool_item = True
-    if condition_bool_item:
-        num = random.randint(0, 1000000)
+from .models import RestrauntLocation
+
+def restraunt_listview(request):
+    template_name = 'restraunts/restraunts_list.html'
+    queryset = RestrauntLocation.objects.all()
     context = {
-        "bool_item": True,
-        "num": num, 
-        "some_list": some_list
+        "object_list": queryset
     }
-    return render(request, "home.html", context) 
-
-def about(request):
-    context = {
-    }
-    return render(request, "about.html", context) 
-    
-def contact(request):
-    context = {
-    }
-    return render(request, "contact.html", context) 
-  
-class HomeView(TemplateView):
-    template_name = "home.html"  
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(HomeView, self).get_context_data(*args, **kwargs)
-        num = None
-        some_list = [
-            random.randint(0, 1000000), 
-            random.randint(0, 1000000), 
-            random.randint(0, 1000000)
-        ]
-        condition_bool_item = True
-        if condition_bool_item:
-            num = random.randint(0, 1000000)
-        context = {
-            "bool_item": True,
-            "num": num, 
-            "some_list": some_list
-        }
-        return context
-
-# class AboutView(TemplateView):
-#     template_name = "about.html"
-
-# class ContactView(TemplateView):
-#     template_name = "contact.html"
+    return render(request, template_name, context)
